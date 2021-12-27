70f6fed536b52ec36c8dd0764805425431dbfc18
hr_hassan_raza <hraza20000@gmail.com>
2021-12-27 14:10:01 +0500

    Final Complete

--- src/App.js
+++ src/App.js
@@ -15,12 +15,15 @@ function App() {
   const [turns, setTurns] = useState(0);
   const [choiceOne, setChoiceOne] = useState(null);
   const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
   // shuffle cards
   const shuffleCards = () => {
     const shuffleCards = [...cardImages, ...cardImages]
       .sort(() => Math.random() - 0.5)
       .map((card) => ({ ...card }));
     setCards(shuffleCards);
+    setChoiceOne(null);
+    setChoiceOne(null);
     setTurns(0);
   };
 
@@ -32,6 +35,7 @@ function App() {
   // comapring 2 selected cards
   useEffect(() => {
     if (choiceOne && choiceTwo) {
+      setDisabled(true);
       if (choiceOne.src === choiceTwo.src) {
         setCards((prevCards) => {
           return prevCards.map((card) => {
@@ -49,14 +53,19 @@ function App() {
     }
   }, [choiceOne, choiceTwo]);
 
-  console.log(cards);
   // reset states of choiceOne and choiceTwo
 
   const resetTurn = () => {
     setChoiceOne(null);
     setChoiceTwo(null);
     setTurns((prevTurns) => prevTurns + 1);
+    setDisabled(false);
   };
+
+  useEffect(() => {
+    shuffleCards();
+  }, []);
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
@@ -69,9 +78,11 @@ function App() {
             card={card}
             handleChoice={handleChoice}
             flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
           ></SingleCard>
         ))}
       </div>
+      <p>Turns: {turns}</p>
     </div>
   );
 }
